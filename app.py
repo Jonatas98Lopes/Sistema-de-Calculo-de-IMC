2d278a09a697329f842bc6c9b7be0ea8f8fa4ab0
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-20 15:50:08 -0300

    interface

--- app.py
+++ app.py
@@ -1,4 +1,48 @@
-altura = float(input("Digite sua altura: "))
+import PySimpleGUI as sg 
+
+# Interface gráfica
+
+def interface():
+
+    sg.theme('Reddit')
+    
+    layout = [
+        [
+            sg.Text('Altura(em centímetros)', size=(20,1)),
+            sg.Input(key='height', size=(3,1))
+        ],
+
+        [
+            sg.Text('Peso(em kilos)', size=(20,1)),
+            sg.Input(key='weight',  size=(3,1))
+            
+        ],
+
+        [sg.Text(key='imc_category',
+                 font=('', 18, 'bold'), 
+                 text_color='#fba900',
+                 justification='center')
+        ],
+
+        [sg.Text(key='imc',
+                 font=('', 14, 'bold'), 
+                 justification='center')],
+                 
+        [sg.Button('Calcular')]
+    ]
+
+    return sg.Window('IMC', layout=layout)
+
+
+window = interface()
+window.read()
+
+
+
+
+
+
+""" altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
 imc = peso / (altura ** 2)
 
@@ -18,4 +62,4 @@ else:
     print('Obesidade Grau III')
 
 
-print(f'IMC: {imc:.2f}')
+print(f'IMC: {imc:.2f}') """
